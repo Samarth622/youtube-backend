c57b4679449e2de56436437e8cf66495fcd36f80
Samarth622 <guptasamarth709@gmail.com>
2024-09-03 01:13:06 +0530

    Custom Handlers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is listen on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MOngodb connection failed !!! ", err);
+  });
